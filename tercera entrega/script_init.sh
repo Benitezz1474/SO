a522fdf5d87c8328bdada7158a4716e5d17dbb46
fernando benitez <benitezz1474@outlook.com>
2025-10-30 21:17:12 -0300

    se contectan los scripts perteneciente a cada directorio, con el scrip principal (script_init)

--- tercera entrega/script_init.sh
+++ tercera entrega/script_init.sh
@@ -7,10 +7,10 @@ while [[ "$op" != 0 ]]
 do
 echo "";
 echo "";
-echo "1) Respaldos manuales del sistema";
-echo "2) Gestion de la base de datos";
-echo "3) comming son";
-echo "4) comming son";
+echo "1) Respaldos y restauraciones manuales de la base de datos";
+echo "2) Gestionar la base de datos";
+echo "3) Gestionar el uso de recursos (VM y contenedores)";
+echo "4) Visualizar logs del servidor (APACHE)";
 echo "0) salir";
 echo "";
 read op;
@@ -19,6 +19,8 @@ case "$op" in
 
 1) RESPALDOS/backupBBDD.sh;;
 2) GESTIONES_DML/scriptDML_tables.sh;;
+3) MONITOREO/script_monitoreo.sh;;
+4) GESTIONES_APACHE/apacheLogs.sh;;
 0) op=0;;
 *) echo "debe ingresar una opcion correcta";;
 
